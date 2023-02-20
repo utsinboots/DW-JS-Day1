0512fe9424ce3dd404dc55221a5f5a79997e0b8c
github username <github email address>
2023-02-20 16:37:43 +0545

    Day1

---
+++ day1.js
@@ -0,0 +1,5 @@
+let num1 = parseInt(document.getElementById("num1").value)
+let num2 = parseInt(document.getElementById("num2").value)
+let result = num1 + num2
+
+document.getElementById("result").innerHTML = "<b>Result = </b>" + result;
