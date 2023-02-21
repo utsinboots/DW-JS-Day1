58430da4c9fd687d3c0052784cd8911ecdf3377e
github username <github email address>
2023-02-21 17:05:39 +0545

    JS-Day 2

--- day1.js
+++ day1.js
@@ -1,5 +1,40 @@
-let num1 = parseInt(document.getElementById("num1").value)
-let num2 = parseInt(document.getElementById("num2").value)
-let result = num1 + num2
+function add(){
+    let num1 = parseInt(document.getElementById("num1").value)
+    let num2 = parseInt(document.getElementById("num2").value)
+    let result = num1 + num2
 
-document.getElementById("result").innerHTML = "<b>Result = </b>" + result;
+    document.getElementById("result").innerHTML = "<b>Result = </b>" + result;
+}
+
+function subtract(){
+    let num1 = parseInt(document.getElementById("num1").value)
+    let num2 = parseInt(document.getElementById("num2").value)
+    let result = num1 - num2
+
+    document.getElementById("result").innerHTML = "<b>Result = </b>" + result;
+}
+
+function multiply(){
+    let num1 = parseInt(document.getElementById("num1").value)
+    let num2 = parseInt(document.getElementById("num2").value)
+    let result = num1 * num2
+
+    document.getElementById("result").innerHTML = "<b>Result = </b>" + result;
+}
+
+/*
+function sayHi()
+{
+    console.log("Hi");
+}
+function sayHello()
+{
+    console.log("Hello");
+}
+*/
+add()
+subtract()
+multiply()
+
+//sayHi()
+//sayHello()
