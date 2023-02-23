734ffe511bc8484803c1d4ae0055c52a2a47594e
github username <github email address>
2023-02-23 15:05:31 +0545

    Day1 end

--- day1.js
+++ day1.js
@@ -7,17 +7,18 @@ function add(){
 }
 
 function subtract(){
-    let num1 = parseInt(document.getElementById("num1").value)
-    let num2 = parseInt(document.getElementById("num2").value)
-    let result = num1 - num2
+    let num3 = parseInt(document.getElementById("num3").value)
+    let num4 = parseInt(document.getElementById("num4").value)
+    let result = num3 - num4
 
     document.getElementById("result").innerHTML = "<b>Result = </b>" + result;
+    
 }
 
 function multiply(){
-    let num1 = parseInt(document.getElementById("num1").value)
-    let num2 = parseInt(document.getElementById("num2").value)
-    let result = num1 * num2
+    let num5 = parseInt(document.getElementById("num5").value)
+    let num6 = parseInt(document.getElementById("num6").value)
+    let result = num5 * num6
 
     document.getElementById("result").innerHTML = "<b>Result = </b>" + result;
 }
